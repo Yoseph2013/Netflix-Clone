898b378bb95a41d38bd3658f2f11def55b8da124
Yoseph <yosephalpha2023@gmail.com>
2024-11-13 19:21:55 -0800

    indentation

--- netflix-clone/Frontend/src/Pages/SignUpPage.jsx
+++ netflix-clone/Frontend/src/Pages/SignUpPage.jsx
@@ -1,4 +1,3 @@
-
 import { Link } from 'react-router-dom'; // Import Link for navigation
 
 function SignUpPage() {
@@ -9,25 +8,26 @@ function SignUpPage() {
           <img src="/netflix-logo.png" alt="Logo" className="w-52" />
         </Link>
       </header>
-      <div className='flex justify-center items-center mt-20 mx-3'>
-        <div className='w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md'>
-        <h1 className='text-center text-white text-2xl font-bold mb-4'> Sign Up </h1>
-        <form className='space-y-4'>
+      
+      <div className="flex justify-center items-center mt-20 mx-3">
+        <div className="w-full max-w-md p-8 space-y-6 bg-black/60 rounded-lg shadow-md">
+          <h1 className="text-center text-white text-2xl font-bold mb-4">Sign Up</h1>
+          
+          <form className="space-y-4">
             <div>
-              <label htmlFor='email' className='text-sm font-medium text-gray-300 block'> 
+              <label htmlFor="email" className="text-sm font-medium text-gray-300 block">
                 Email
               </label>
               <input
-                placeholder='you@example.com ' 
-                type='email' 
-                id='email'
-                className= 'w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring'/>
-                
-              </div>
-        </form>
-
+                placeholder="you@example.com"
+                type="email"
+                id="email"
+                className="w-full px-3 py-2 mt-1 border border-gray-700 rounded-md bg-transparent text-white focus:outline-none focus:ring"
+              />
+            </div>
+          </form>
+          
         </div>
-
       </div>
     </div>
   );
