a3ca0808295d5e788157ae23fbe11f958f492d92
Yoseph <yosephalpha2023@gmail.com>
2024-11-13 19:19:01 -0800

    Signup

--- netflix-clone/Frontend/src/Pages/HomePage.jsx
+++ netflix-clone/Frontend/src/Pages/HomePage.jsx
@@ -1,10 +1,7 @@
 
 function HomePage() {
   return (
-    <div>
-      <h1 className="text-3xl font-bold underline text-green-700">
-     Home Page
-    </h1>
+    <div className="hero-bg h-screen">   
     </div>
   )
 }
