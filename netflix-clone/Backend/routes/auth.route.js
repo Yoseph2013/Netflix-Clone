f7a8485fa7a483d6a027a82e531c4fb7c7fb3f7d
Yoseph <yosephalpha2023@gmail.com>
2024-11-15 16:45:53 -0800

    route

--- netflix-clone/Backend/routes/auth.route.js
+++ netflix-clone/Backend/routes/auth.route.js
@@ -1,15 +1,20 @@
 import express from 'express';
+import {protectRoute} from '../middleware/protectRoute.js'
 
 import { login,
          logout,
-         signup
+         signup,
+         authCheck,
      } from '../controller/auth.controller.js';
+
 
 const router = express.Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
+
+router.get("/authCheck", protectRoute,authCheck)
 
 export default router;
 
